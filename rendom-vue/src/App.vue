6d7706bbc43255dae57808105ede317d5499de76
SimoneScordato99 <scordato.simone@yahoo.com>
2023-11-22 10:37:24 +0100

    start + header

---
+++ rendom-vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { store } from "./store.js";
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <section class="appp">
+    <Header></Header>
+  </section>
+</template>
+
+<style scoped>
+  .appp{
+    width: 100vw;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    height: 100vh;
+  }
+</style>
---
+++ rendom-vue/src/components/Header.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { store } from "../store.js";
+</script>
+
+<template>
+    <div class="box">
+        <div class="box-logo">
+            <img src="../../public/Immagine 2023-11-21 160218.png" alt="logo">
+        </div>
+        <div class="box-navigazione">
+            <ul>
+                <li>mario</li>
+                <li>mario</li>
+                <li>mario</li>
+                <li>mario</li>
+                <li>mario</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .box{
+        width: 100%;
+        height: 200px;
+        .box-logo{
+            height: 200px;
+            img{
+                height: 100%;
+            }
+        }
+        .box-navigazione{
+            background-color: black;
+            color: white;
+            
+            ul{
+                display: flex;
+                justify-content: space-around;
+                margin: 0;
+                padding: 5px 0;
+                li{
+                    list-style-type: none;
+                    text-transform: uppercase;
+
+                }
+            }
+        }
+    }
+</style>
